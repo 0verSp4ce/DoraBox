58e5e1f89a711cb565d74fb3c809742e1694168a
key <412391882@qq.com>
2017-12-19 17:14:50 +0800

    Edit

--- class/function.class.php
+++ class/function.class.php
@@ -8,6 +8,7 @@ class Func {
 	var $form_method;
 	var $form_params;
 	
+	
 	public function __construct($form_method='', $form_params=''){
 		//构造函数，对象创建时获取form表单的相关传参，以便con_html方法生成
 		$this -> form_method = $form_method;
@@ -33,15 +34,17 @@ class Func {
 	}
 
 	public function con_mysql($t_name, $c_name, $c_value, $sql_type){
+		//mysql
 		$conn = @mysql_connect(DB_HOST, DB_USER, DB_PASS) or die('bad!');
 		mysql_query("SET NAMES utf-8");
 		mysql_select_db(DB_NAME, $conn) OR emMsg("数据库连接失败");
-		$sql_num = "SELECT * FROM {$t_name} WHERE {$c_name} = {$c_value}";
-		$sql_string = "SELECT * FROM {$t_name} WHERE {$c_name} = '{$c_value}'";
-		$sql_search = "SELECT * FROM {$t_name} WHERE {$c_name} like '%{$c_value}%'";
+		$sql_num = "SELECT * FROM {$t_name} WHERE {$c_name} = {$c_value}"; //num
+		$sql_string = "SELECT * FROM {$t_name} WHERE {$c_name} = '{$c_value}'"; //string
+		$sql_search = "SELECT * FROM {$t_name} WHERE {$c_name} like '%{$c_value}%'"; //search
 		$sql_name = "sql_".$sql_type;
 		$result = mysql_query($$sql_name, $conn) or die(mysql_error());
 		$row = mysql_fetch_array($result, MYSQL_ASSOC);
+		mysql_close($conn);
 		return $row;
 	}
 
