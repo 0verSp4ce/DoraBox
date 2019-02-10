2dc6b7d44ac3a014d65c504517bdb17d37a81f8e
evilos <gh0stkey>
2019-02-10 21:59:20 +0800

    update

--- csrf/userinfo.php
+++ csrf/userinfo.php
@@ -5,6 +5,7 @@
 		header("Access-Control-Allow-Origin: *");
 	}
 	header("Access-Control-Allow-Headers: X-Requested-With");
+	header("Access-Control-Allow-Credentials: true");
 	header("Access-Control-Allow-Methods: PUT,POST,GET,DELETE,OPTIONS");
 
 	$info = array('username' => 'Vulkey_Chen', 'mobilephone' => '13188888888', 'email' => 'admin@gh0st.cn', 'address' => '中华人民共和国', 'sex' => 'Cool Man');
